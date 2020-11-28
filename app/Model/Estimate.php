be423c23340acc3a658c3df546ef213ff34e6797
FI12LHO <marlonemarques0@gmail.com>
2020-11-28 14:42:19 -0300

    Oficina 2.0 - Laravel CRUD

--- app/Model/Estimate.php
+++ app/Model/Estimate.php
@@ -3,10 +3,10 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Estimate extends Model
 {
-    //
     protected $primaryKey = 'id';
     
     protected $table = 'estimate';
@@ -18,4 +18,15 @@ class Estimate extends Model
     protected $guarded = [
         'id', 'created_at', 'update_at'
     ];
+
+    /**
+     * @param filter = Nome da coluna que ordena a busca
+     * @param paginate = Quantidade de registros obtidos e retornados por busca
+     * @return = os registros do BD filtrados por coluna
+     */
+
+    static function filterAllData($filter = 'date', $paginate = 15) {
+        $data = DB::table('estimate') -> orderByDesc($filter) -> paginate($paginate);
+        return $data;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,14 @@ Route::group(['prefix' => '/'], function () {
     // Create
     Route::get('/create', 'estimateController@create');
     Route::post('/create', 'estimateController@store');
+    
     // Read
     Route::get('/', 'estimateController@index');
+
     // Update
     Route::get('/show/{id}', 'estimateController@show');
     Route::put('/edit/{id}', 'estimateController@edit');
+
     // Delete
     Route::get('/delete/{id}', 'estimateController@delete');
     Route::delete('/destroy/{id}', 'estimateController@destroy');
